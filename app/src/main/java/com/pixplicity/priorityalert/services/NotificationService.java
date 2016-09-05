4921b4c2776fc9c200b82c58aeb95687e98c49be
Paul Lammertsma <pflammertsma@gmail.com>
2016-09-05 09:57:37 +0200

    experimenting with using all accessibility event types

--- app/src/main/java/com/pixplicity/priorityalert/services/NotificationService.java
+++ app/src/main/java/com/pixplicity/priorityalert/services/NotificationService.java
@@ -3,8 +3,10 @@ package com.pixplicity.priorityalert.services;
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.Notification;
+import android.os.Build;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
 
 import com.pixplicity.priorityalert.receivers.PriorityReceiver;
 
@@ -17,32 +19,50 @@ public class NotificationService extends AccessibilityService {
     protected void onServiceConnected() {
         Log.d(TAG, "connected");
         super.onServiceConnected();
-        if (false) {
-            AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-            info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-            info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
-            info.notificationTimeout = 100;
-            setServiceInfo(info);
-            Log.d(TAG, "set service info");
+
+        final AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType |= AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+        info.feedbackType |= AccessibilityServiceInfo.FEEDBACK_AUDIBLE;
+        info.feedbackType |= AccessibilityServiceInfo.FEEDBACK_HAPTIC;
+        info.flags |= AccessibilityServiceInfo.DEFAULT;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            info.flags |= AccessibilityServiceInfo.FLAG_REQUEST_ENHANCED_WEB_ACCESSIBILITY;
+            info.flags |= AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS;
+            info.flags |= AccessibilityServiceInfo.FLAG_REQUEST_FILTER_KEY_EVENTS;
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            info.flags |= AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS;
+        }
+        info.notificationTimeout = 0;
+        setServiceInfo(info);
+        Log.d(TAG, "set service info");
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         Log.d(TAG, "accessibility event: " + event);
-        Notification notification = (Notification) event.getParcelableData();
-        CharSequence title = notification.extras.getCharSequence(Notification.EXTRA_TITLE);
-        if (title != null) {
-            Log.d(TAG, title.toString());
-        }
-        CharSequence[] lines = notification.extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
-        if (lines != null) {
-            for (CharSequence msg : lines) {
-                Log.d(TAG, msg.toString());
+        if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+            Notification notification = (Notification) event.getParcelableData();
+            CharSequence title;
+            if (notification == null) {
+                title = "(unknown)";
+            } else {
+                title = notification.extras.getCharSequence(Notification.EXTRA_TITLE);
+                if (title != null) {
+                    Log.d(TAG, title.toString());
+                }
+                CharSequence[] lines = notification.extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
+                if (lines != null) {
+                    for (CharSequence msg : lines) {
+                        Log.d(TAG, msg.toString());
+                    }
+                }
+                if (PriorityReceiver.isRelevant(this, title, lines)) {
+                    PriorityReceiver.invoke(this);
+                }
             }
-        }
-        if (PriorityReceiver.isRelevant(this, title, lines)) {
-            PriorityReceiver.invoke(this);
+            Toast.makeText(this, "Notification: " + title, Toast.LENGTH_LONG).show();
         }
     }
 
